d811bd1fce95232b67c69505eb450857787c172b
verenvalenciaa <verenvalenciaa@gmail.com>
2019-04-18 21:52:50 +0700

    tes

--- admin/hasilApprovePinjam.php
+++ admin/hasilApprovePinjam.php
@@ -1,4 +1,48 @@
-peminjaman anda berhasil diterima! 
-<a href="index.php">Kembali ke halaman utama</a>
-<br>
-<a href="pendingLab.php">Lihat peminjaman lab yang pending lainnya</a>
+<html>
+    <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Click &amp; Rent &mdash; SISTech UPH </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="../fonts/fontawesome/css/font-awesome.min.css">    
+     <!-- Theme Style -->
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+    <center>
+         <body  class="site-hero" style="background-image: url(../images/thx-bg.jpg);overflow-x:hidden; overflow-y:hidden; "data-stellar-background-ratio="0.5">
+        <table style= "margin-top:70px;">
+           <tr>
+               <td align="center" colspan="2">
+                   <h1>Your reservation has been confirmed!</h1>
+               </td>
+            </tr>
+            <tr>
+               <td align="center" colspan="2">
+                   <img src="../images/approve.png" width="200">
+               </td>
+            </tr>
+            <br>
+            <tr align="center">
+                <td>
+                    <a href="pendingLab.php" class="btn btn-danger">Lihat peminjaman lab yang pending lainnya</a>
+                </td>
+                 <td>
+                    <a href="pendingLab.php" class="btn btn-danger">Lihat peminjaman meeting room yang pending lainnya</a>
+                </td>
+            </tr>
+            <tr align="center">
+                <td colspan="2">
+                    <a href="index.php" class="btn btn-danger">Kembali ke halaman utama</a>
+                </td>
+            </tr>
+        </table>
+        </body>
+        </center>
+</html>
