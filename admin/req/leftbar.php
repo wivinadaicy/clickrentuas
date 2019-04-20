49153f42343575ee99c4cfe7355f23c4afe77191
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2019-04-20 23:02:05 +0700

    Adghfdgf

--- admin/req/leftbar.php
+++ admin/req/leftbar.php
@@ -274,13 +274,14 @@
 			
 					</ul>
 					<?php }else if($data['status_pengguna']=="2"){ ?>
-					<ul class="nav nav-main">
-						<li class="nav-active">
-							<a href="index.html">
+						<ul class="nav nav-main">
+						<li>
+							<a href="index.php">
 								<i class="fa fa-home" aria-hidden="true"></i>
 								<span>Dashboard</span>
 							</a>
 						</li>
+                        <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
 						<li>
 						<a href="pesan.php">
 							<?php
@@ -296,9 +297,29 @@
 							<?php } ?>
 
 							<i class="fa fa-envelope" aria-hidden="true"></i>
-							<span>Pesan</span>
+							<span>Messages</span>
 						</a>
 						</li>
+
+						<li>
+						<a href="contactMessage.php">
+							<?php
+							$ceknotifmail = mysqli_query($koneksi, "SELECT count(id_contact) FROM contact WHERE status_pesan='0'");
+
+							$datamail = mysqli_fetch_array($ceknotifmail);
+							$jumlah = $datamail['count(id_contact)'];
+							if($jumlah!=0){
+?>
+							<span class="pull-right label label-primary"><?php echo 
+							$jumlah?></span>
+
+							<?php } ?>
+
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+							<span>Contact Messages</span>
+						</a>
+						</li>
+
 						<li class="nav-parent">
 							<a>
 								<i class="fa fa-copy" aria-hidden="true"></i>
@@ -306,13 +327,33 @@
 							</a>
 							<ul class="nav nav-children">
 								<li>
-									<a href="">
-										 Rooms &amp; Types of Rooms
+									<a href="pengguna.php">
+										 Users
 									</a>
 								</li>
 								<li>
-									<a href="">
-										 Items &amp; Types of Items
+									<a href="mahasiswa.php">
+										 Students
+									</a>
+								</li>
+								<li>
+									<a href="prodi.php">
+										 Major
+									</a>
+								</li>
+                                <li>
+									<a href="fakultas.php">
+										 Faculty
+									</a>
+								</li>
+								<li>
+									<a href="ruangan.php">
+										 Rooms
+									</a>
+								</li>
+								<li>
+									<a href="barang.php">
+										 Items
 									</a>
 								</li>
 								<li>
@@ -322,12 +363,28 @@
 								</li>
 							</ul>
 						</li>
+						
+						<li>
+							<a href="memberPending.php">
+								<i class="fa fa-home" aria-hidden="true"></i>
+								<span>New Member Pending</span>
+								<?php
+								$ceknotifmail = mysqli_query($koneksi, "SELECT count(pengguna.id_pengguna) FROM pengguna WHERE status_daftar= '1'");
+
+								$datamail = mysqli_fetch_array($ceknotifmail);
+								$jumlah = $datamail['count(pengguna.id_pengguna)'];
+								if($jumlah!=0){
+									?>
+									   &nbsp;&nbsp;&nbsp;<span class="badge"><?php echo $jumlah?></span>
+									<?php } ?>
+							</a>
+						</li>
 
 						<hr class="separator" />
 
 						<li class="nav-parent">
 							<a>
-								<i class="fa fa-copy" aria-hidden="true"></i>
+								<i class="fa fa-calendar" aria-hidden="true"></i>
 								
 								<span>Schedule</span>
 							</a>
@@ -339,7 +396,7 @@
 									</a>
 								</li>
 								<li>
-									<a href="">
+									<a href="jadwalMeeting.php">
 										 Meeting Room
 									</a>
 								</li>
@@ -348,9 +405,9 @@
 						<li class="nav-parent">
 							<a>
 								<i class="fa fa-copy" aria-hidden="true"> </i>
-								<span>Pending Reservation</span>
+								<span>Pending Reservation </span>
 								<?php
-								$ceknotifmail = mysqli_query($koneksi, "SELECT count(peminjaman.id_peminjaman) FROM peminjaman WHERE peminjaman.status_peminjaman ='0' or peminjaman.status_peminjaman='5'");
+								$ceknotifmail = mysqli_query($koneksi, "SELECT count(peminjaman.id_peminjaman) FROM peminjaman WHERE peminjaman.status_peminjaman ='0'");
 
 								$datamail = mysqli_fetch_array($ceknotifmail);
 								$jumlah = $datamail['count(peminjaman.id_peminjaman)'];
@@ -401,25 +458,43 @@
 							<ul class="nav nav-children">
 								<li>
 									<a href="peminjamanAdminLab.php">
-										 Laboratory
-										 
+                                        Laboratory
 									</a>
 								</li>
 								<li>
-									<a href="">
+									<a href="peminjamanAdminMeeting.php">
+										 Meeting Room
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-parent">
+							<a>
+								<i class="fa fa-copy" aria-hidden="true"></i>
+								
+								<span>Finished Reservation</span>
+							</a>
+							<ul class="nav nav-children">
+								<li>
+									<a href="peminjamanAdminSelesaiLab.php">
+                                        Laboratory
+									</a>
+								</li>
+								<li>
+									<a href="peminjamanAdminSelesaiMeet.php">
 										 Meeting Room
 									</a>
 								</li>
 							</ul>
 						</li>
 						<li>
-							<a href="">
+							<a href="logPeminjaman.php">
 								<i class="fa fa-home" aria-hidden="true"></i>
-								<span>Finished Reservation</span>
+								<span>Reservation Log</span>
 							</a>
 						</li>
-						<hr class="separator">
-<li>
+                        <hr class="separator">
+                        <li>
 							<a href="peminjamanMemberAkanDatang.php">
 								<i class="fa fa-home" aria-hidden="true"></i>
 								<span>Upcoming Reservation</span>
@@ -433,13 +508,13 @@
 						</li>
 						<li>
 							<a href="peminjamanMemberDibatalkan.php">
-								<i class="fa fa-home" aria-hidden="true"></i>
+								<i class=" fa fa-times-circle-o" aria-hidden="true"></i>
 								<span>Cancelled Reservation</span>
 							</a>
 						</li>
 						<li>
 							<a href="peminjamanMemberSelesai.php">
-								<i class="fa fa-home" aria-hidden="true"></i>
+								<i class=" fa fa-check-circle-o" aria-hidden="true"></i>
 								<span>Finished Reservation</span>
 							</a>
 						</li>
