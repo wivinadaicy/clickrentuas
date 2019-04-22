770ff248a79d1fb6a1676637c2bbc9efe24311b5
verenvalenciaa <verenvalenciaa@gmail.com>
2019-04-22 16:10:56 +0700

    kategori acara

--- admin/fakultas/modaldetail.php
+++ admin/fakultas/modaldetail.php
@@ -2,7 +2,7 @@
     <section class="panel">
         <form class="form-horizontal mb-lg" method="post">
         <header class="panel-heading">
-            <h2 class="panel-title">Details of faculty</h2>
+            <h2 class="panel-title">Details of Faculty</h2>
         </header>
         <div class="panel-body">
 		<div class="form-group mt-lg">
